9a631e2331ee3f79e42785d26c361e8c963fff58
kerem212012 <rockstarfoxykerem@gmail.com>
2025-10-26 19:31:55 +0300

    Add subject management and user score features
    Introduces functionality to add and delete subjects, view and filter quizzes by subject, and display user scores. Updates UI elements and callback handlers to support these features, and refines quiz answer handling for subject-specific quizzes.

--- lgs/bot.py
+++ lgs/bot.py
@@ -31,8 +31,11 @@ def start_question(message, subject):
     user_state[user_id] = "question"
     bot.send_message(user_id, text="Soru'nun fotosunu at!")
 
-
-
+def start_subject(message):
+    user_id = message.chat.id
+    user_data[user_id] = {}
+    user_state[user_id] = "s_name"
+    bot.send_message(user_id, text="Ders adını yaz:")
 
 @bot.message_handler(func=lambda msg: msg.chat.id in user_state,content_types=['photo','text'])
 def handle_add_steps(message):
@@ -68,11 +71,21 @@ def handle_add_steps(message):
         code_btn = types.InlineKeyboardButton(text="Geri", callback_data="panel")
         markup.row(code_btn)
         bot.send_message(message.chat.id, "Soruyu ekledin!", reply_markup=markup)
-
+    if state == "s_name":
+        user_data[user_id]["s_name"] = message.text.strip()
+        user_state[user_id] = "description"
+        bot.send_message(user_id, text="Açıklamasını yaz yaz:")
+    if state == "description":
+        user_data[user_id]["description"] = message.text.strip()
+        del user_state[user_id]
+        info = user_data[user_id]
+        Subject.objects.create(title=info["s_name"],description=info["description"])
+        code_btn = types.InlineKeyboardButton(text="Geri", callback_data="panel")
+        markup.row(code_btn)
+        bot.send_message(message.chat.id, "Ders ekledin!", reply_markup=markup)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, f"{message.photo} a")
     markup = types.InlineKeyboardMarkup()
     create_user(message.chat.id, message.chat.first_name)
     bot.send_message(message.chat.id, text=f"Merhaba!{message.chat.first_name}")
@@ -105,9 +118,15 @@ def callback_handler(call):
         markup = types.InlineKeyboardMarkup()
         add_q_btn = types.InlineKeyboardButton(text="Soru Ekle", callback_data="add_q")
         del_q_btn = types.InlineKeyboardButton(text="Soru Sil", callback_data="list_q")
+        add_s_btn = types.InlineKeyboardButton(text="Ders Ekle", callback_data="add_s")
+        del_s_btn = types.InlineKeyboardButton(text="Ders Sil", callback_data="list_s")
+        score_btn = types.InlineKeyboardButton(text="Birisi'nin Skoruna Bak", callback_data="list")
         back_btn = types.InlineKeyboardButton(text="Back", callback_data="main")
         markup.row(add_q_btn)
         markup.row(del_q_btn)
+        markup.row(add_s_btn)
+        markup.row(del_s_btn)
+        markup.row(score_btn)
         markup.row(back_btn)
         bot.send_message(call.message.chat.id, "Bir şey seç:", reply_markup=markup)
     if call.data == "list_q":
@@ -119,32 +138,84 @@ def callback_handler(call):
         back_btn = types.InlineKeyboardButton(text="Back", callback_data="panel")
         markup.row(back_btn)
         bot.send_message(call.message.chat.id, "Silmek için seç:", reply_markup=markup)
+    if call.data == "list_s":
+        markup = types.InlineKeyboardMarkup()
+        for subject in Subject.objects.all():
+            del_s_btn = types.InlineKeyboardButton(text=f"{subject.title}", callback_data=f"del_s|{subject.id}")
+            markup.row(del_s_btn)
+        back_btn = types.InlineKeyboardButton(text="Back", callback_data="panel")
+        markup.row(back_btn)
+        bot.send_message(call.message.chat.id, "Silmek için seç:", reply_markup=markup)
+    if call.data == "list":
+        markup = types.InlineKeyboardMarkup()
+        for user in CustomUser.objects.all():
+            if user.tg_id == call.message.chat.id:
+                btn = types.InlineKeyboardButton(text=f"Siz", callback_data=f"score|{user.id}")
 
+            else:
+                btn = types.InlineKeyboardButton(text=f"{user.first_name}", callback_data=f"score|{user.id}")
+            markup.row(btn)
+        bot.send_message(call.message.chat.id, "Kullanıcı Seçin:", reply_markup=markup)
+    if call.data.split("|", 1)[0] == "score":
+        markup = types.InlineKeyboardMarkup()
+        user=CustomUser.objects.get(id=call.data.split("|", 1)[1])
+        back_btn = types.InlineKeyboardButton(text="Geri", callback_data="panel")
+        markup.row(back_btn)
+        bot.send_message(call.message.chat.id, f"{user.first_name}\nYanlışlar:{user.wrongs}\nDoğrular:{user.corrects}", reply_markup=markup)
     if call.data.split("|", 1)[0] == "del_q":
         markup = types.InlineKeyboardMarkup()
         question=Quiz.objects.get(id=call.data.split("|", 1)[1])
         question.delete()
-        back_btn = types.InlineKeyboardButton(text="Back",callback_data="panel")
+        back_btn = types.InlineKeyboardButton(text="Geri",callback_data="panel")
         markup.row(back_btn)
         bot.send_message(call.message.chat.id, "Soruyu sildin!", reply_markup=markup)
+    if call.data.split("|", 1)[0] == "del_s":
+        markup = types.InlineKeyboardMarkup()
+        subject=Subject.objects.get(id=call.data.split("|", 1)[1])
+        subject.delete()
+        back_btn = types.InlineKeyboardButton(text="Geri",callback_data="panel")
+        markup.row(back_btn)
+        bot.send_message(call.message.chat.id, "Ders sildin!", reply_markup=markup)
 
     if call.data == "start":
         markup = types.InlineKeyboardMarkup()
+        for subject in Subject.objects.all():
+            btn = types.InlineKeyboardButton(text=subject.title, callback_data=f"filter|{subject.id}")
+            markup.row(btn)
         btn = types.InlineKeyboardButton(text="Her şey", callback_data=f"quiz")
         markup.row(btn)
         bot.send_message(call.message.chat.id, "Bir şey seç:", reply_markup=markup)
     if call.data == "quiz":
         markup = types.InlineKeyboardMarkup()
         question = random.choice(Quiz.objects.all())
+        symbols = ["A","B","C","D"]
         answers = [question.wrong_answer_1, question.right_answer, question.wrong_answer_2, question.wrong_answer_3]
-        for _ in range(4):
-            answer = random.choice(answers)
+        for i in range(len(answers)):
+            for j in range(len(symbols)):
+                if answers[j] == symbols[i]:
+                    answer = answers[j]
+
             if answer == question.right_answer:
                 btn = types.InlineKeyboardButton(text=answer, callback_data=f"right|{question.id}")
             else:
                 btn = types.InlineKeyboardButton(text=answer, callback_data=f"wrong|{question.id}")
             markup.row(btn)
-            answers.remove(answer)
+        bot.send_photo(call.message.chat.id, photo=question.question, reply_markup=markup)
+    if call.data.split("|", 1)[0] == "filter":
+        markup = types.InlineKeyboardMarkup()
+        question = random.choice(Quiz.objects.filter(subject=call.data.split("|", 1)[1]))
+        symbols = ["A", "B", "C", "D"]
+        answers = [question.wrong_answer_1, question.right_answer, question.wrong_answer_2, question.wrong_answer_3]
+
+        for i in range(len(answers)):
+            for j in range(len(symbols)):
+                if answers[j] == symbols[i]:
+                    answer = answers[j]
+            if answer == question.right_answer:
+                btn = types.InlineKeyboardButton(text=answer, callback_data=f"f_right|{question.id}")
+            else:
+                btn = types.InlineKeyboardButton(text=answer, callback_data=f"f_wrong|{question.id}")
+            markup.row(btn)
         bot.send_photo(call.message.chat.id, photo=question.question, reply_markup=markup)
     if call.data.split("|", 1)[0] == "right":
         markup = types.InlineKeyboardMarkup()
@@ -165,12 +236,38 @@ def callback_handler(call):
         user = CustomUser.objects.get(tg_id=call.message.chat.id)
         user.wrongs += 1
         user.save()
-        continue_btn = types.InlineKeyboardButton(text="Continue", callback_data=f"quiz")
-        quit_btn = types.InlineKeyboardButton(text="Quit", callback_data="main")
+        continue_btn = types.InlineKeyboardButton(text="Devam et", callback_data=f"quiz")
+        quit_btn = types.InlineKeyboardButton(text="Çık", callback_data="main")
         markup.row(continue_btn)
         markup.row(quit_btn)
         bot.send_message(call.message.chat.id,
                          text=f"Hayır!Yanlış yaptın doğru cevap:{question.right_answer}",
+                         reply_markup=markup)
+    if call.data.split("|", 1)[0] == "f_wrong":
+        markup = types.InlineKeyboardMarkup()
+        question = Quiz.objects.get(id=call.data.split("|", 1)[1])
+        user = CustomUser.objects.get(tg_id=call.message.chat.id)
+        user.wrongs += 1
+        user.save()
+        continue_btn = types.InlineKeyboardButton(text="Devam et", callback_data=f"filter|{question.subject.id}")
+        quit_btn = types.InlineKeyboardButton(text="Çık", callback_data="main")
+        markup.row(continue_btn)
+        markup.row(quit_btn)
+        bot.send_message(call.message.chat.id,
+                         text=f"Hayır!Yanlış yaptın doğru cevap:{question.right_answer}",
+                         reply_markup=markup)
+    if call.data.split("|", 1)[0] == "f_right":
+        markup = types.InlineKeyboardMarkup()
+        question = Quiz.objects.get(id=call.data.split("|", 1)[1])
+        user = CustomUser.objects.get(tg_id=call.message.chat.id)
+        user.corrects += 1
+        user.save()
+        continue_btn = types.InlineKeyboardButton(text="Devam et", callback_data=f"filter|{question.subject.id}")
+        quit_btn = types.InlineKeyboardButton(text="Çık", callback_data="main")
+        markup.row(continue_btn)
+        markup.row(quit_btn)
+        bot.send_message(call.message.chat.id,
+                         text=f"Tebrikler!Doğru yaptın cevap:{question.right_answer}",
                          reply_markup=markup)
     if call.data == "add_q":
         markup = types.InlineKeyboardMarkup()
@@ -180,6 +277,7 @@ def callback_handler(call):
         bot.send_message(call.message.chat.id, "Bir ders seç:", reply_markup=markup)
     if call.data.split("|", 1)[0] == "add_q_2":
         start_question(call.message, call.data.split("|", 1)[1])
-
+    if call.data == "add_s":
+        start_subject(call.message)
 
 bot.infinity_polling()
